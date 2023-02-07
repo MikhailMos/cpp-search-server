276ce37edab7634671b4274d4a2212f351721b28
Mikhail Moskvin <mos-mv@mail.ru>
2023-02-07 16:57:24 +0300

    first commit to the project

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,34 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+// Напишите ответ здесь: 271
+#include <string>
+#include <iostream>
+
+int main(int nNumberofArgs, char *pszArgs[])
+{
+    using namespace std;
+
+    int count_result = 0;
+
+    for (int i = 1; i <= 1000; i++)
+    {
+        if (i < 3)
+        {
+            continue;
+        }
+        string strNumber = to_string(i);
+        for (char tmpChar : strNumber)
+        {
+            if (tmpChar == '3')
+            {
+                ++count_result;
+                break;
+            }
+        }
+    }
+
+    cout << "Result is "s << count_result << endl;
+
+    return 0;
+}
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
